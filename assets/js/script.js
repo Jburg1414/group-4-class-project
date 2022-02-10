0c22719c0cabd4b0caade6716fb68df91676c384
Jburg1414 <Jburgess1414@gmail.com>
2022-02-09 22:24:44 -0700

    cleaned up code and was working on phone number history from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-var phoneNumber = document.getElementById("inputnumber").value;
-
 var verifyphone = function () {
   var phonenumber = document.getElementById("inputnumber").value;
   console.log(phonenumber);
@@ -26,59 +24,64 @@ var verifyphone = function () {
       console.error(err);
     });
 
-  saveNumbers(phonenumber);
+    var saveNumbers = JSON.parse(localStorage.getItem("saveNumbers")) ?? [];
+    saveNumbers.push(phonenumber);
+    localStorage.setItem("saveNumbers", JSON.stringify(saveNumbers));
+    console.log(localStorage);
+
+    var numbersHistory = document.getElementById("getNumber");
+    numbersHistory.innerHTML = "";
+    for (var i = 0; i < saveNumbers.length; i++) {
+        var phoneHistory = document.createElement("button");
+        phoneHistory.type = "submit";
+        phoneHistory.onclick = searchPhone;
+        phoneHistory.appendChild(document.createTextNode(saveNumbers[i]));
+        numbersHistory.appendChild(phoneHistory);
+    }
 };
+
+
+function searchPhone() {
+    console.log(this);
+    var phonenumber = $(this).text();
+    verifyphone(phonenumber);
+}
 
 var verifybutton = document.getElementById("btn");
 verifybutton.addEventListener("click", verifyphone);
 
 function createDiv() {
-//   let div = document.createElement("div");
-//   div.innerText = document.getElementById("getText").innerText;
-//   document.body.appendChild(div);
-document.getElementById("getText").classList.remove("hidden");
+    document.getElementById("getText").classList.remove("hidden");
 }
-
-// load numbers for the first time
-// loadNumbers();
-
-// load phone numbers from local storage
-// var loadNumbers = function() {
-//     numbers = JSON.parse(localStorage.getItem("numbers"));
-// };
-
-var saveNumbers = function (phonenumero) {
-  localStorage.setItem("numbers", phonenumero);
-};
 
 // function to get weather based on phone number location
 var getWeather = function () {
-  var numberLocation = $("#location").text().split(":")[1];
-  console.log(numberLocation);
-
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${numberLocation}&appid=8cab3bb02def5dd471eb0acffee86b41&units=imperial`;
-
-  fetch(apiUrl)
+    var numberLocation = $("#location").text().split(":")[1];
+    console.log(numberLocation);
+    
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${numberLocation}&appid=8cab3bb02def5dd471eb0acffee86b41&units=imperial`;
+    
+    fetch(apiUrl)
     .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        return response.json();
-      }
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }
     })
     .then(function (data) {
-      console.log(data);
-      var date = new Date(data.dt*1000).toLocaleDateString();
-      var weather = ` <div class="box column is-6">
-      <h3>${date}</h3>
-      <p>Current Temp: ${data.main.temp} ℉</p>
-      <p>Current Wind Speed: ${data.wind.speed} MPH</p>
-      <p>Current Humidity: ${data.main.humidity} %</p>
-  </div>`
-
-  document.getElementById("getText").innerHTML = weather;
+        console.log(data);
+        var date = new Date(data.dt*1000).toLocaleDateString();
+        var weather = ` <div class="box column is-6">
+        <h3>${date}</h3>
+        <p>Current Temp: ${data.main.temp} ℉</p>
+        <p>Current Wind Speed: ${data.wind.speed} MPH</p>
+        <p>Current Humidity: ${data.main.humidity} %</p>
+        </div>`
+        
+        document.getElementById("getText").innerHTML = weather;
     })
     .catch(function (error) {
-      console.log(error);
+        console.log(error);
     });
 };
 
